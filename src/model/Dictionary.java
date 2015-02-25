7207dfe7e7d71df8acc0566f291fe73773c5d733
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-02-25 10:59:06 +0100

    Clean some code

--- src/model/Dictionary.java
+++ src/model/Dictionary.java
@@ -10,8 +10,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
