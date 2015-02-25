cc48bbdd0a49dea225e4dfd83cdcfbb5f4a99b9b
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-02-25 11:55:21 +0100

    Can read/save list with file

--- src/model/Config.java
+++ src/model/Config.java
@@ -10,4 +10,5 @@ public class Config {
     public static String folderName = "data";
     public static String wordFileName = "word";
     public static String linkFileName = "link";
+    public static String listFileName = "list";
 }
